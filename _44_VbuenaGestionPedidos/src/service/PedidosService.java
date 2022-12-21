251b4e4079a637d815f44626e9aae8a8c7a4b576
kevin <kevin@PanDora>
2022-12-21 21:12:08 +0100

    Subido con mis comentarios version buena GestionDePedidos

--- _44_VbuenaGestionPedidos/src/service/PedidosService.java
+++ _44_VbuenaGestionPedidos/src/service/PedidosService.java
@@ -1,11 +1,12 @@
 package service;
-//ENUNCIADO: Programa para gestión de pedidos
-//Version corregida
 
 import java.util.ArrayList;
+
 import java.util.Date;
 
 import model.Pedidos;
+//ENUNCIADO: Programa para gestión de pedidos
+//Version corregida
 
 //1.- Nuevo pedido
 //2.- Pedido más reciente
@@ -17,25 +18,25 @@ public class PedidosService {
 	
 	ArrayList<Pedidos> pedido= new ArrayList<>();
 	
-	public void nuevoPedido (String producto, Date fecha, int total) {
-		pedido.add(new Pedidos(producto,fecha,total));
+	public void nuevoPedido (String producto, Date fecha, int total) {//metodo para agregar pedido
+		pedido.add(new Pedidos(producto,fecha,total)); //añadimos a pedido nuevo objeto Pedidos 
 	}
-	public Pedidos masReciente() { 
-		Pedidos pedidos=null;
-		Date fechaReciente=new Date(0);
-		for(Pedidos p: pedido) {
-			if(p.getFecha().after(fechaReciente)) {
-				fechaReciente=p.getFecha();
-				pedidos=p;
+	public Pedidos masReciente() { //metodo para mostrar el producto mas reciente
+		Pedidos pedidos=null; //declaramos la variable con null/nada
+		Date fechaReciente=new Date(0);//la variable con formato fecha con la posicion 0
+		for(Pedidos p: pedido) { //recoremos el ArrayList 
+			if(p.getFecha().after(fechaReciente)) {//Preguntamos si el valor del atributo getFecha es mas reciente que fechaReciente
+				fechaReciente=p.getFecha();//añadimos a fechaReciente p.getFecha
+				pedidos=p;//por donde paso p en el ArrayList lo añadimos a pedidos
 			}
 		}
-		return pedidos;
+		return pedidos; //devolvemos pedidos
 	}		
-	public Pedidos [] todos() {
-		Pedidos[] resultado=new Pedidos[pedido.size()];
-		for(int i=0;i<pedido.size();i++) {
-			resultado[i]=pedido.get(i);
+	public Pedidos[] todos() {//metodo para mostrar los datos de todos los pedidos
+		Pedidos[] resultado=new Pedidos[pedido.size()];//creamos la variable resultado con el tamaño que tiene pedido
+		for(int i=0;i<pedido.size();i++) {//Recoremos Array que no sea mayor que pedido
+			resultado[i]=pedido.get(i);//en cada posicion de resultado que se creo metemos lo que encontro al recorrer (i)
 		}
-		return resultado;
+		return resultado;//devolvemos resultado 
 	}
 }
