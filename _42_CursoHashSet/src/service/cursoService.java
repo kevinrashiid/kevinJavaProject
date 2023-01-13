f092349866704a0a37dc6f80c99a4284885904f4
manana <manana@A13E31>
2023-01-13 14:00:44 +0100

    Merge branch 'master' of https://github.com/kevinrashiid/kevinJavaProject.git
    desde clase

--- _42_CursoHashSet/src/service/cursoService.java
+++ _42_CursoHashSet/src/service/cursoService.java
@@ -19,24 +19,38 @@ Utilizamos HashSet para guardar los cursos*/
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 import model.CursosModel;
 
 public class cursoService {
+	//empezamos el codigo haciendo la estructura de todos los metodos por ahora vacios
 	
 	HashSet<CursosModel> Cursos=new HashSet<>();
 	
-	public void guardarCurso(String nombreCurso, double dMediaCurso, int precio, String tematica) {
+	public boolean guardarCurso(CursosModel cur) {
 		for(CursosModel n:Cursos) {
-			if(!Cursos.equals(n.getNombreCurso())){//no se si esta bien
-				Cursos.add(new Cs(nombreCurso,dMediaCurso,precio,tematica));
+			//si encontramos un curso con el mismo nombre salimos y devolvemos false
+			if(n.getNombreCurso().equalsIgnoreCase(n.getNombreCurso())){//no se si esta bien
+				return false;
 			}
 		}
+		//si llegamos aqui es por que no hay un curso con el mismo nombre
+		Cursos.add(cur);
+		return true;
 	}
-	public ArrayList<CursosModel> buscarCurso() {//2.- Buscar curso //Metodo creado para buscar 
-		ArrayList<CursosModel> c=new ArrayList<>();
+	public CursosModel buscarCurso(String nombreCurso) {//2.- Buscar curso //Metodo creado para buscar 
+		/*ArrayList<CursosModel> c=new ArrayList<>();
 		for(CursosModel k:Cursos) {
-			c.add(k);
+			c.add(k); 
+		}
+		return c;*/ 
+		CursosModel c=null;
+		for(CursosModel cr:Cursos) {
+			if(cr.getNombreCurso().equalsIgnoreCase(nombreCurso)) {//encontrado
+				c=cr;
+				break;
+			}
 		}
 		return c;
 	}
@@ -45,26 +59,26 @@ public class cursoService {
 		for(CursosModel c: Cursos) {
 			media=media+c.getdMediaCurso();
 		}
-		return media;
+		return media/Cursos.size();
 	}
-	public ArrayList<String> tematicaCurso(String tematica) {
-		ArrayList<String> res=new ArrayList<>();
+	public List<String> tematicaCurso(String tematica) {
+		List<String> res=new ArrayList<>(); //creamos un ArrayList de tipo List // Arraylist es una sublista de List 
 		for(CursosModel c:Cursos) {
+			//si la tematica del curso que recorremos es igual a la temática recibida, lo añadimos a la lista auxiliar
 			if(c.getTematica().equalsIgnoreCase(tematica)) {
 				res.add(c.getNombreCurso());
 			}
 		}
-		return res;
+		return res; 
 	}
-	public int eliminarCurso(int precio){
-	int cont=0;
+	public void eliminarCurso(int precio){
+	/*int cont=0;
 	for(CursosModel s:Cursos) {
 		if(s.getPrecio()>precio) {
 			Cursos.remove(s); 
 			cont++;
-			
 			}
-		}	
-	return cont;
+		}*/
+		Cursos.removeIf(t->t.getPrecio()>precio);
 	}
 }	
---
+++ _42_CursoHashSet/src/service/cursoServiceFicheros.java
@@ -0,0 +1,72 @@
+package service;
+/*ENUNCIADO DE EJERCICIO:
+
+1.- Añadir curso
+2.- Buscar curso
+3.- Duración media de cursos
+4.- Cursos por temática
+5.- Eliminar cursos por precio
+6.- Salir
+
+Un curso se caracteriza por nombre, duracion, precio y temática.
+Opción 1: se solicitan los datos del curso y se añade (opcional, no debería permitir cursos con el mismo nombre)
+Opción 2: Se pide el nombre del curso y se muestran todos los datos del mismo
+Opción 3: Muestra la duración media de todos los cursos registrados
+Opción 4: Se pìde una temática y se muestran  datos de todos los cursos que tengan esa temática
+Opción 5: Se pide un precio y se eliminarán todos los cursos cuyo precio sea igual o superior a dicho valor
+
+Utilizamos HashSet para guardar los cursos*/
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.List;
+
+import model.CursosModel;
+
+public class cursoServiceFicheros {
+	//empezamos el codigo haciendo la estructura de todos los metodos por ahora vacios
+	String ruta="c:\\temp\\cursos.txt";
+	
+	
+	public boolean guardarCurso(CursosModel cur) {
+		try(FileReader fr=new FileReader(ruta);
+				BufferedReader bf=new BufferedReader(fr);){
+				String s;
+				while((s=bf.readLine())!=null) {
+					//si encontramos curso con dicho nombre, salimos
+					if(s.split("[|]")[0].equals(CursosModel.getNombreCurso())) {
+						return false;
+					}
+				}
+			}catch(IOException ex) {
+				ex.printStackTrace();
+			}
+			//aqui llega si no encuentra curso, y entonces lo tiene
+			//que grabar en el fichero
+			try(FileOutputStream fos=new FileOutputStream(ruta,true);
+				PrintStream out=new PrintStream(fos);){
+				out.println(CursosModel.getNombreCurso()+"|"+CursosModel.getPrecio()+"|"+CursosModel.getdMediaCurso()+"|"+CursosModel.getTematica());
+				
+			}catch(IOException ex) {
+				ex.printStackTrace();
+			}
+			return true;
+			
+		}
+	public CursosModel buscarCurso(String nombreCurso) {//2.- Buscar curso //Metodo creado para buscar 
+	
+	}
+	public double duracionMediaCursos() {//3.- Duración media de cursos// Metodo creado para mostrar la duracion media de todos los cursos registrados
+		
+	}
+	public List<String> tematicaCurso(String tematica) {
+	
+	}
+	public void eliminarCurso(int precio){//5.- Eliminar cursos por precio
+
+
+	}
+}
