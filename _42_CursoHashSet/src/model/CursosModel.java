cafd2524095befa84e6efcd87fe4c5b7a8f32ccf
manana <manana@A13E31>
2023-01-18 12:41:59 +0100

    subido desde clase

--- _42_CursoHashSet/src/model/CursosModel.java
+++ _42_CursoHashSet/src/model/CursosModel.java
@@ -1,40 +1,19 @@
 package model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//@Data //es como pusieras @Setter @Getter
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CursosModel {
 	private String nombreCurso;
 	private double dMediaCurso;
 	private int precio;
 	private String tematica;
-	public CursosModel(String nombreCurso, double dMediaCurso, int precio, String tematica) {
-		
-		this.nombreCurso = nombreCurso;
-		this.dMediaCurso = dMediaCurso;
-		this.precio = precio;
-		this.tematica = tematica;
-	}
-	public String getNombreCurso() {
-		return nombreCurso;
-	}
-	public void setNombreCurso(String nombreCurso) {
-		this.nombreCurso = nombreCurso;
-	}
-	public double getdMediaCurso() {
-		return dMediaCurso;
-	}
-	public void setdMediaCurso(double dMediaCurso) {
-		this.dMediaCurso = dMediaCurso;
-	}
-	public int getPrecio() {
-		return precio;
-	}
-	public void setPrecio(int precio) {
-		this.precio = precio;
-	}
-	public String getTematica() {
-		return tematica;
-	}
-	public void setTematica(String tematica) {
-		this.tematica = tematica;
-	}
 }
 
